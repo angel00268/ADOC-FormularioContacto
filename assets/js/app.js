dd6ec6fbb71027e6ccb2e744c4703671733cbca1
Angel Sebastián Saravia Serpas <angel.saravia@salud.gob.sv>
2025-01-16 21:48:24 -0600

    fix: corrigiendo validacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#btnEnviar").on("click", function(e) {
+    $("#btnEnviar").on("click", function (e) {
         if (validar('frmContacto', 'input, textarea')) {
             $.ajax({
                 type: 'post',
@@ -15,7 +15,7 @@ $(document).ready(function () {
                     $("#alert-danger").css('display', 'none');
                 },
                 success: function (data) {
-                    if(manageError(data.error)){
+                    if (manageError(data.error)) {
                         if (data.message != null && data.error == null) {
                             $("#alert-success").css('display', 'flex')
                             $("#alert-success span").html(`<h6> <strong> <i class="fa fa-check-circle"></i> Información: </strong> ${data.message} </h6>`).show();
@@ -42,13 +42,13 @@ function validar(seccion, elements) {
     $(`#${seccion} label[for]`).css('color', 'black');
     $(`#${seccion} span[class="error_span"]`).text('');
     elementos.each(function (index, element) {
-        let elemento        = $(element);
-        let id              = elemento.attr('id');
-        let type            = elemento.attr('type');
-        let label           = $(`label[for="${id}"]`)
-        let labelText       = label.text();
-        let errorSpan       = $(`span[id="error_${id}"]`)
-        let valInput        = elemento.val();
+        let elemento = $(element);
+        let id = elemento.attr('id');
+        let type = elemento.attr('type');
+        let label = $(`label[for="${id}"]`)
+        let labelText = label.text();
+        let errorSpan = $(`span[id="error_${id}"]`)
+        let valInput = elemento.val();
         let dataCaracterMin = elemento.data('min');
         let dataCaracterMax = elemento.data('max');
         if (elemento.is(':required') && !(valInput)) {
@@ -57,28 +57,24 @@ function validar(seccion, elements) {
             elemento.focus();
             valido = false;
             errorSpan.html(`El campo <strong> ${labelText} </strong> es requerido`)
-            return false;
         } else if (elemento.is(':required') && typeof dataCaracterMin !== undefined && valInput.trim().length < parseInt(dataCaracterMin)) {
             label.css('color', '#ff2d00');
             elemento.addClass('has-warning');
             elemento.focus();
             valido = false;
             errorSpan.html(`El campo <strong> ${labelText} </strong> debe tener al menos <strong> ${dataCaracterMin} </strong> caracteres.`)
-            return false;
         } else if (elemento.is(':required') && typeof dataCaracterMax !== undefined && valInput.trim().length > parseInt(dataCaracterMax)) {
             label.css('color', '#ff2d00');
             elemento.addClass('has-warning');
             elemento.focus();
             valido = false;
             errorSpan.html(`El campo <strong> ${labelText} </strong> no debe tener más de <strong> ${dataCaracterMax} </strong> caracteres.`)
-            return false;
-        } else if (type === 'email' &&  !emailRegex.test(valInput)) {
+        } else if (type === 'email' && !emailRegex.test(valInput)) {
             label.css('color', '#ff2d00');
             elemento.addClass('has-warning');
             elemento.focus();
             valido = false;
             errorSpan.html(`El campo <strong> ${labelText} </strong> debe de ser un formato de correo electronico.`)
-            return false;
         }
     });
     return valido;
@@ -86,7 +82,7 @@ function validar(seccion, elements) {
 
 function manageError(error) {
     console.log(error);
-    
+
     if (error != null) {
         $errores = '';
         let tipo = $.type(error);
@@ -100,7 +96,7 @@ function manageError(error) {
         }
         $("#alert-danger").css('display', 'flex');
         $("#alert-danger span").html(`<h6> <strong> Error: </strong><ul>${$errores}</ul>`).show();
-    }else{
+    } else {
         return true;
     }
 }
